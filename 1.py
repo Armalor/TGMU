ef750ea9669fa009fee6595c5bad34eacaea1b1d
NNK <n.kutrukhin@digitalms.ru>
2025-11-18 11:55:16 +0300

    Работа с числами

--- 1.py
+++ 1.py
@@ -1,1 +1,28 @@
-print('Hello world 2!')
+
+s1 = 'Hello world 1!'
+s2 = "Hello world 2!"
+s3 = """
+На краю дороги стоял дуб.
+На краю дуба сидел кот. 
+На краю кота сидел?
+"""
+
+# print(s3)
+#
+# who = input("Кто сидел? Введи, пожалуйста, кто там сидел:")
+#
+# print("Итого, на краю кота сидел", who)
+
+a = input('Enter a:')
+print(type(a))
+a = int(a)
+print(type(a))
+
+pi = 3
+
+b = a / 2
+bb = a // 2
+
+bbb = a ** 3
+
+print(f'{b=}, {bb=} {bbb=}')
