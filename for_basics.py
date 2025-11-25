cdc76529d748eb3125f0b56d8bbded49ddfbe606
NNK <n.kutrukhin@digitalms.ru>
2025-11-25 11:04:58 +0300

    Работа с for и со списком

--- for_basics.py
+++ for_basics.py
@@ -19,3 +19,15 @@ for j in range(10):
     print(j)
 
 print(f'range as is: {range(10)}')
+
+for j in range(11):
+    print(j**3)
+
+# Вывести только четные элементы списка
+for n in lst:
+    if n % 2 == 0:
+        print(n)
+
+# Вывести квадраты ЧЕТНЫХ чисел от 1 до 10
+for j in range(2, 11, 2):
+    print(j**2)
