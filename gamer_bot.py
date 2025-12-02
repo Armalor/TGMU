b579f888fe6be85df48eddafb5ab20e09258f112
NNK <n.kutrukhin@digitalms.ru>
2025-12-02 11:11:38 +0300

    Телеграм-бот, еще заготовка.

--- gamer_bot.py
+++ gamer_bot.py
@@ -7,15 +7,17 @@ class BotConfig:
     def __init__(self):
         self.token = '8464798499:AAHqmqmTCbYE74f5caboD2QCzypH8k9WMck'
         self.url = 't.me/PlayerTGMUBot'
+        self.parse_mode = 'HTML'
 
 
 config = BotConfig()
 
-bot = TeleBot(config.token, parse_mode='HTML')
+bot = TeleBot(config.token, parse_mode=config.parse_mode)
 
 
 @bot.message_handler(commands=['auth'])
 def auth(message):
+
     bot.send_message(
         message.chat.id,
         f"Привет! chat_id = {message.chat.id}; user_id = {message.from_user.id}"
@@ -24,54 +26,28 @@ def auth(message):
 
 @bot.message_handler(commands=['start'])
 def start(message):
+
+    bot.delete_message(
+        chat_id=message.chat.id,
+        message_id=message.id,
+    )
+
     inline_kb = InlineKeyboardMarkup(row_width=2)
-    poker_btn = InlineKeyboardButton('Покер!', callback_data='poker')
-    roulette_btn = InlineKeyboardButton('Рулетка!', callback_data='roulette')
-    slot_btn = InlineKeyboardButton('Однорукий бандит!', callback_data='slot')
+
+    poker_btn = InlineKeyboardButton('Покер', callback_data='poker')
+    roulette_btn = InlineKeyboardButton('Рулетка', callback_data='roulette')
+    slot_btn = InlineKeyboardButton('Однорукий бандит', callback_data='slot')
+
     inline_kb.add(poker_btn, roulette_btn, slot_btn)
 
     exit_btn = InlineKeyboardButton('Не, я на выход', callback_data='exit')
     inline_kb.add(exit_btn)
 
     bot.send_message(
-        message.chat.id,
-        f"Приветствуем вас в нашем казино! Где желаете сыграть?",
+        chat_id=message.chat.id,
+        text=f"Приветствует вас в казино! Во что хотите сыграть?",
         reply_markup=inline_kb
     )
-
-
-@bot.callback_query_handler(func=lambda c: c.data and c.data.startswith('poker'))
-def pocker_callback(callback_query: CallbackQuery):
-    buttons = [
-        KeyboardButton('кнопка 1'),
-        KeyboardButton('кнопка 2'),
-        KeyboardButton('кнопка 3'),
-    ]
-
-    markup = ReplyKeyboardMarkup(one_time_keyboard=True)
-    markup.add(*buttons)
-    bot.send_message(callback_query.message.chat.id, 'Выберите что вам надо', reply_markup=markup)
-
-
-@bot.message_handler(content_types=['text'])
-def message_reply(message):
-    if message.text == "кнопка 1":
-        markup=ReplyKeyboardMarkup(one_time_keyboard=True)
-        item1=KeyboardButton("Кнопка 11")
-        markup.add(item1)
-        bot.send_message(message.chat.id, 'Кнопка 1 →', reply_markup=markup)
-    elif message.text == "кнопка 2":
-        bot.send_message(message.chat.id, 'Спасибо за визит!')
-
-
-@bot.callback_query_handler(func=lambda c: c.data and c.data.startswith('roulette'))
-def roulette_callback(callback_query):
-    print(callback_query)
-
-
-@bot.callback_query_handler(func=lambda c: c.data and c.data.startswith('slot'))
-def slot_callback(callback_query):
-    print(callback_query)
 
 
 @bot.callback_query_handler(func=lambda c: c.data and c.data.startswith('exit'))
@@ -80,6 +56,30 @@ def exit_callback(callback_query):
         chat_id=callback_query.message.chat.id,
         message_id=callback_query.message.id,
     )
+
+
+@bot.callback_query_handler(func=lambda c: c.data and c.data.startswith('slot'))
+def slot_callback(callback_query):
+    b1 = KeyboardButton('автомат 1')
+    b2 = KeyboardButton('автомат 2')
+    b3 = KeyboardButton('автомат 3')
+
+    rkb = ReplyKeyboardMarkup(one_time_keyboard=True)
+
+    rkb.add(b1, b2, b3)
+
+    bot.send_message(callback_query.message.chat.id, 'Выберите автомат, который вам нужен!', reply_markup=rkb)
+
+
+@bot.message_handler(content_types=['text'])
+def replay_handler(message):
+    if message.text == 'автомат 1':
+        bot.send_message(message.chat.id, 'Вы играете на автомате №1')
+    elif message.text == 'автомат 2':
+        rkb = ReplyKeyboardMarkup(one_time_keyboard=True)
+        b2 = KeyboardButton('Крутить барабан автомата 2')
+        rkb.add(b2)
+        bot.send_message(message.chat.id, 'Пошла игра:', reply_markup=rkb)
 
 
 if __name__ == '__main__':
---
+++ lambdas.py
@@ -0,0 +1,12 @@
+# Лямбда-функции:
+
+
+def qube(x):
+    return x * x * x
+
+
+q = lambda x: x * x * x
+
+f = q(5)
+
+print(f)
